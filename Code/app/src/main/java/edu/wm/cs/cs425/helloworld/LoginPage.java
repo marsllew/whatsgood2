3ebc48a0a8d43e63ff44c316facf543eafd38c6a
Marshall Dunn <mldunn@email.wm.edu>
2023-04-10 19:41:04 -0400

    Created a basic login page and added boxes for information, currently nonfunctional, pressing login button moves onto main activity.

--- Code/app/src/main/java/edu/wm/cs/cs425/helloworld/LoginPage.java
+++ Code/app/src/main/java/edu/wm/cs/cs425/helloworld/LoginPage.java
@@ -2,7 +2,11 @@ package edu.wm.cs.cs425.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,6 +14,20 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        TextView username =(TextView) findViewById(R.id.editTextTextPersonName);
+        TextView password =(TextView) findViewById(R.id.editTextTextPassword);
+
+        Button loginbtn = (Button) findViewById(R.id.loginButton);
+
+        loginbtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginPage.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
         //work?
     }
 }
