1c9908f5446c7e2161890f02e33e1fd38c92c8c2
mldunn <mldunn@wm.edu>
2023-04-09 23:24:44 -0400

    Addition of blank login page

---
+++ Code/app/src/main/java/edu/wm/cs/cs425/helloworld/LoginPage.java
@@ -0,0 +1,15 @@
+package edu.wm.cs.cs425.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        //work?
+    }
+}
