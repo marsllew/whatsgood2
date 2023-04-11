e5e6722f9cae05570a45b790d607990a07e85764
Marshall Dunn <mldunn@email.wm.edu>
2023-04-10 21:01:43 -0400

    Finished login page, with full connection to firebase, including registering accounts, default email and password is "a"

--- Code/app/src/main/java/edu/wm/cs/cs425/helloworld/LoginPage.java
+++ Code/app/src/main/java/edu/wm/cs/cs425/helloworld/LoginPage.java
@@ -1,5 +1,6 @@
 package edu.wm.cs.cs425.helloworld;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,26 +9,66 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private TextView emailInput;
+    private TextView passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-
-        TextView username =(TextView) findViewById(R.id.editTextTextPersonName);
-        TextView password =(TextView) findViewById(R.id.editTextTextPassword);
+        mAuth = FirebaseAuth.getInstance();
+        emailInput =(TextView) findViewById(R.id.accountEmail);
+        passwordInput =(TextView) findViewById(R.id.accountPassword);
 
         Button loginbtn = (Button) findViewById(R.id.loginButton);
+        Button newUser = (Button) findViewById(R.id.newUser);
 
         loginbtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(LoginPage.this,MainActivity.class);
-                startActivity(i);
+                loginAttempt();
+
             }
         });
+        newUser.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent reg = new Intent(LoginPage.this,LoginRegistration.class);
+                startActivity(reg);
+            }
+        });
+
         //work?
     }
+
+    private void loginAttempt(){
+        String emailText, passText;
+        emailText = emailInput.getText().toString();
+        passText = passwordInput.getText().toString();
+        if(emailText.equals("a") && passText.equals("a")) {
+            Intent lgn = new Intent(LoginPage.this, MainActivity.class);
+            startActivity(lgn);
+        }
+
+        mAuth.signInWithEmailAndPassword(emailText,passText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent loginSuc = new Intent(LoginPage.this, MainActivity.class);
+                    startActivity(loginSuc);
+                }
+            }
+        });
+    }
+
 }
