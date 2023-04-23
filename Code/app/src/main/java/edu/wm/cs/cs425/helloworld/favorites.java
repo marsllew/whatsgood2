076b2f6f63093682e84e2f288b283562d69f81fc
bnvieitez <bnvieitez@wm.edu>
2023-04-23 16:54:21 -0400

    favorites tab recycle view

--- Code/app/src/main/java/edu/wm/cs/cs425/helloworld/favorites.java
+++ Code/app/src/main/java/edu/wm/cs/cs425/helloworld/favorites.java
@@ -8,6 +8,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 
 public class favorites extends Fragment {
@@ -16,8 +20,20 @@ public class favorites extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_reviews, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+
+
+        ReviewModel rvMenu = new ReviewModel("Salad", "Sadler");
+        ArrayList<ReviewModel> rvList = new ArrayList<>();
+        rvList.add(rvMenu);
+        RecyclerView recyclerView = view.findViewById(R.id.reviewRecycle);
+        RVAdapter menuadapt = new RVAdapter(getContext(), rvList);
+        LinearLayoutManager llmMenu = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(llmMenu);
+        recyclerView.setAdapter(menuadapt);
+
+        return view;
     }
 
     private String getUserID (){
