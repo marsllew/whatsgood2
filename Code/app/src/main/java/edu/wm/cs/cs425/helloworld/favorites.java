7b7d74ae005918a6c7084613351c7a0e4a393efa
mldunn <mldunn@wm.edu>
2023-04-23 16:41:38 -0400

    Added the function to pull the current User ID to the favorites page.

--- Code/app/src/main/java/edu/wm/cs/cs425/helloworld/favorites.java
+++ Code/app/src/main/java/edu/wm/cs/cs425/helloworld/favorites.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.fragment.app.Fragment;
 
 
 public class favorites extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,4 +19,10 @@ public class favorites extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorites, container, false);
     }
+
+    private String getUserID (){
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        return currentUser.getUid();
+    }
+
 }
