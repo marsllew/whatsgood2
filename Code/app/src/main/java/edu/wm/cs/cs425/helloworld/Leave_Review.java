0c0775cc58c0ae1520a6ae609ac18fc132b29080
Marshall Dunn <mldunn@email.wm.edu>
2023-04-30 12:17:10 -0400

    Basic Review system works, stores user information and food information, as well as leaving it easily identified by user. Check Firestore for data layout.

--- Code/app/src/main/java/edu/wm/cs/cs425/helloworld/Leave_Review.java
+++ Code/app/src/main/java/edu/wm/cs/cs425/helloworld/Leave_Review.java
@@ -17,7 +17,11 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Leave_Review extends AppCompatActivity {
 
@@ -40,10 +44,11 @@ public class Leave_Review extends AppCompatActivity {
         String location = extras.getString("location");
         int rating = extras.getInt("rating");
         int pic = extras.getInt("image");
-
+        String corlocation = location.replace("/", " or ");
+        Log.d(TAG, corlocation);
         TextView foodT = findViewById(R.id.Sampletxt1), locationT = findViewById(R.id.Sampletxt);
         foodT.setText(food);
-        locationT.setText(location);
+        locationT.setText(corlocation);
 
         ImageView image = findViewById(R.id.review_pic);
         image.setImageResource(R.drawable.food);
@@ -51,8 +56,9 @@ public class Leave_Review extends AppCompatActivity {
         submit.setOnClickListener(view -> {
             String text = reviewText.getText().toString();
 
-            Review review = new Review(rating, text, food, location);
+            Review review = new Review(rating, text, food, corlocation);
             uploadReview(review);
+            //problem
         });
     }
 
@@ -62,6 +68,43 @@ public class Leave_Review extends AppCompatActivity {
     }
 
     public void uploadReview(Review review) {
+        Map<String, Object> foodReview = new HashMap<>();
+        foodReview.put("location", review.getLocation());
+        foodReview.put("food", review.getFood());
+        foodReview.put("username", getName());
+        foodReview.put("text", review.getText());
+        foodReview.put("rating", review.getRating());
+
+        db.collection("Reviews")
+                .document("Sadler")
+                .collection(review.getLocation())
+                .document(review.getFood())
+                .collection("reviews")
+                .document(user.getUid())
+                .set(foodReview)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                });
+
+        Map<String, Object> ratingStore = new HashMap<>();
+        ratingStore.put("rating", review.getRating());
+        db.collection("Reviews")
+                .document("Sadler")
+                .collection(review.getLocation())
+                .document(review.getFood())
+                .set(ratingStore)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                });
+
+
+        /***
         db.collection("Reviews")
                 .document(review.getLocation())
                 .collection(review.getFood())
@@ -79,6 +122,7 @@ public class Leave_Review extends AppCompatActivity {
                         Log.w(TAG, "Error writing document", e);
                     }
                 });
+         ***/
     }
 
     private String getName(){
