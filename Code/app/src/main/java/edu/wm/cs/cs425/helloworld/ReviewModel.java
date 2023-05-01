47beee32a0cfd3b56fec739939f9090646d9bd65
tcook <tcook@email.wm.edu>
2023-04-30 20:14:40 -0400

    Added stock photos into dictionary to be used with the station it is at. Not fully implemented, but the dictionary is in

--- Code/app/src/main/java/edu/wm/cs/cs425/helloworld/ReviewModel.java
+++ Code/app/src/main/java/edu/wm/cs/cs425/helloworld/ReviewModel.java
@@ -1,5 +1,10 @@
 package edu.wm.cs.cs425.helloworld;
 
+import android.media.Image;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class ReviewModel {
     String FoodName;
     String FoodLocation;
@@ -8,11 +13,39 @@ public class ReviewModel {
 
     String calories;
 
+    Dictionary<String, Integer> image_dict = new Hashtable<>();
+
+
 
     public ReviewModel(String foodName, String foodLocation, String cal) {
+        image_dict.put("DELI", R.drawable.turkeysliced);
+        image_dict.put("DESSERT", R.drawable.cupcakes);
+        image_dict.put("GRILL", R.drawable.cheesburger);
+        image_dict.put("SALAD BAR", R.drawable.salad);
+        image_dict.put("SALAD", R.drawable.salad);
+        image_dict.put("MACTOWN", R.drawable.macandcheese);
+        image_dict.put("KALAMATA", R.drawable.kalamata);
+        image_dict.put("SIMPLE SERVINGS", R.drawable.cornbread);
+        image_dict.put("STARCH", R.drawable.rice);
+        image_dict.put("CONDIMENT/GARNISH", R.drawable.condiment);
+        image_dict.put("HEART & STONE", R.drawable.cheesepizza);
+        image_dict.put("ENTREE", R.drawable.chickenburger);
+        image_dict.put("VEGETABLE", R.drawable.romaine);
+        image_dict.put("WOK", R.drawable.wok);
+        image_dict.put("BAKERY", R.drawable.cookies);
+        image_dict.put("BREAKFAST", R.drawable.oamlet);
+        image_dict.put("COMFORT", R.drawable.comfort);
+        image_dict.put("SOUP", R.drawable.soup);
+        image_dict.put("CUSTO-MEX", R.drawable.tacos);
+        image_dict.put("GLOBAL KITCHEN", R.drawable.pasta);
+        image_dict.put("SADLER'S DELI", R.drawable.turkeysliced);
+        image_dict.put("SWEET SPOT", R.drawable.cupcakes);
+        image_dict.put("ACTION - MADE TO ORDER", R.drawable.comfort);
+        image_dict.put("-", R.drawable.comfort);
+        image_dict.put("APPETIZER", R.drawable.appetizer);
         this.FoodName = foodName;
         this.FoodLocation = foodLocation;
-        this.image = image;
+        this.image = image_dict.get(this.FoodLocation);
         this.calories = cal;
     }
 
