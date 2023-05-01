d899057231f3707a23e89a109845fb0d48d795f3
Ilhan Farah Mohamud <ifmohamud@email.wm.edu>
2023-04-30 23:42:17 -0400

    This is for the notifications if your favorites food is on the menu

--- Code/app/src/main/java/edu/wm/cs/cs425/helloworld/favorites.java
+++ Code/app/src/main/java/edu/wm/cs/cs425/helloworld/favorites.java
@@ -5,28 +5,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class favorites extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favorites, container, false);
-        // Inflate the layout for this fragment
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         ArrayList<ReviewModel> rvList = new ArrayList<>();
         RecyclerView recyclerView = view.findViewById(R.id.favoritesRecycle);
@@ -44,14 +44,33 @@ public class favorites extends Fragment {
                         String calories = document.getString("calories");
                         Log.d("retrieve", foodName + locationName + calories);
                         rvList.add(new ReviewModel(foodName, locationName, calories));
+                        boolean isFavoriteFoodOnMenu = false;
+                        List<String> favoriteFoods = getFavoriteFoods();
+                        Log.d("retrieve", "Favorite food: " + favoriteFoods);
+                        for (ReviewModel reviewModel : rvList) {
+                            Log.d("retrieve", "Review model food: " + reviewModel.getFoodName());
+                            if (reviewModel.getFoodName().equals(favoriteFoods)) {
+                                Log.d("retrieve", "Review model food: " + reviewModel.getFoodName());
+                                isFavoriteFoodOnMenu = true;
+                                Log.d("retrieve", "Match found");
+                                break;
+                            }
+                        }
+
+
+                        if (isFavoriteFoodOnMenu) {
+                            Toast.makeText(getContext(), "One or more of your favorite foods are on the menu!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getContext(), "None of your favorite foods are on the menu.", Toast.LENGTH_SHORT).show();
+                        }
+
                     }
                     menuadapt.notifyDataSetChanged();
+
                 })
                 .addOnFailureListener(e -> {
                     Log.w("Firestore", "could not retrieve data from database");
                 });
-//        ReviewModel rvMenu = new ReviewModel("Salad", "Sadler", "0 Cals");
-//        rvList.add(rvMenu);
 
         return view;
     }
@@ -61,4 +80,21 @@ public class favorites extends Fragment {
         return currentUser.getUid();
     }
 
+    private List<String> getFavoriteFoods() {
+        List<String> favoriteFoods = new ArrayList<>();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference favoritesRef = db.collection("users").document(getUserID()).collection("favorites");
+        favoritesRef.get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    List<DocumentSnapshot> documents = queryDocumentSnapshots.getDocuments();
+                    for (DocumentSnapshot document : documents) {
+                        String foodName = document.getString("foodname");
+                        favoriteFoods.add(foodName);
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.w("Firestore", "could not retrieve data from database");
+                });
+        return favoriteFoods;
+    }
 }
